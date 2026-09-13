831f9ead28e9d70799fb97aafd8eed2cf573a759
alex <8379108+Alex33856@users.noreply.github.com>
2026-09-13 15:37:35 -0400

    feat: make View Only the default role, add role requests

--- WebInterface/accounts/models.py
+++ WebInterface/accounts/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -23,3 +24,8 @@ class CustomUser(AbstractUser):
 
     def role_color(self) -> str:
         return ROLE_COLORS.get(self.role, "danger")
+
+
+class RoleRequest(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role_name = models.CharField(max_length=20, choices=ROLES)
--- WebInterface/accounts/views.py
+++ WebInterface/accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 
-from .models import CustomUser
+from .models import CustomUser, RoleRequest
 
 
 # Handles storing registration info and sending new user to dashboard page
@@ -12,14 +12,18 @@ def register(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        role = request.POST['role']
+        email = request.POST['email']
+        requested_role = request.POST['role']
 
         if CustomUser.objects.filter(username=username).exists():
             messages.error(request, 'Username is already taken!', extra_tags='danger')
-            context = {"prefillUser": username, "prefillPassword": password}
+            context = {"prefillUser": username, "prefillEmail": email, "prefillPassword": password}
             return render(request, 'register.html', context)
 
-        user = CustomUser.objects.create_user(username=username, password=password, role=role)
+        user = CustomUser.objects.create_user(username=username, password=password, email=email, role='view_only',
+                                              is_viewer=True)
+        RoleRequest.objects.create(user=user, role_name=requested_role)
+
         messages.success(request, "Account created successfully!")
         login(request, user)
         return redirect('dashboard')
@@ -55,7 +59,7 @@ def logout_view(request):
 def demo_login(request):
     from accounts.models import CustomUser
     try:
-        demo_user = CustomUser.objects.get(username='showcase', is_viewer=True)
+        demo_user = CustomUser.objects.get(username='showcase', is_viewer=True, role='view_only')
         login(request, demo_user, backend='django.contrib.auth.backends.ModelBackend')
         return redirect('experiment_run_dynamic', experiment_name='CartControl')
     except CustomUser.DoesNotExist:
